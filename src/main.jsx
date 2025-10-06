bb0de98e0f9b30ab458a1eead43baf963c189e00
Rafael <ansdles@live.com>
2025-10-06 13:59:58 -0300

    feat: cria desisão de desenvolvimento local ou nuvem

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,12 @@ import './index.css'
 import { db } from './firebaseConfig' //Importa nossa conexão com o DB
 import { connectFirestoreEmulator } from 'firebase/firestore' //Importa a função do emulador
 
+
 //Conecta ao emulador local do Firestore que está rodando na porta 8080
-connectFirestoreEmulator(db, '127.0.0.1', 8080);
+if (import.meta.env.DEV) {//SE estiver no ambiente de DESENVOLVIMENTO
+  connectFirestoreEmulator(db, '127.0.0.1', 8080);
+  console.log("App conectado aos emuladores do Firebase.");
+}
 //--- FIM DAS ADIÇÕES ---
 
 ReactDOM.createRoot(document.getElementById('root')).render(
